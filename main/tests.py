de54b7f39f8109b6352dfb8669111796c23c4f3a
Alexander <alexndegwa46@gmail.com>
2022-02-28 14:59:35 +0300

    procfile added

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from models import category, Image
 # Create your tests here.
+
+#set up method
+def setUp(self):
+    self.user=category('title')
+    self.user=Image('description', 'alttext', 'hashtags')
+
+#testing instance
+def test_instance(self):
+    self.assertTrue(isinstance(self.user))
