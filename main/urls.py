93def13b7e986368a14188107c9f07cfb3106dbe
Alexander <alexndegwa46@gmail.com>
2022-02-28 11:01:21 +0300

    urls linking done in main app

--- main/urls.py
+++ main/urls.py
@@ -19,3 +19,17 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    #Main App Views
+    path('',include('main.urls')),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
